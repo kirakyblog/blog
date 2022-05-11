f0630f9193ab75a769d6cfe7ad1ade5cc9dd979d
R <rifky060796@gmail.com>
2022-05-11 10:25:48 +0700

    Update serviceworker.js

--- logo/serviceworker.js
+++ logo/serviceworker.js
@@ -1,4 +1,3 @@
-const js = `
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 if (workbox) {
 workbox.core.skipWaiting();
@@ -9,13 +8,10 @@ workbox.core.setCacheNameDetails({
   precache: 'install-time',
   runtime: 'run-time'
 });
-
 const FALLBACK_HTML_URL = 'https://offline.kiraky.workers.dev/';
 const version = workbox.core.cacheNames.suffix;
 workbox.precaching.precacheAndRoute([{url: FALLBACK_HTML_URL, revision: null},{url: '/manifest.json', revision: null},{url: '/main/favicon.ico', revision: null}]);
-
 workbox.routing.setDefaultHandler(new workbox.strategies.NetworkOnly());
-
 workbox.routing.registerRoute(
     new RegExp('.(?:css|js|png|gif|jpg|svg|ico)$'),
     new workbox.strategies.CacheFirst({
@@ -29,7 +25,6 @@ workbox.routing.registerRoute(
         ],
     }),'GET'
 );
-
 workbox.routing.setCatchHandler(({event}) => {
       switch (event.request.destination) {
         case 'document':
@@ -39,7 +34,6 @@ workbox.routing.setCatchHandler(({event}) => {
         return Response.error();
   }
 });
-
 self.addEventListener('activate', function(event) {
   event.waitUntil(
     caches
@@ -48,12 +42,11 @@ self.addEventListener('activate', function(event) {
       .then(keys => Promise.all(keys.map(key => caches.delete(key))))
   );
 });
-
 }
 else {
     console.log('Oops! Workbox did not load');
 }
-`
+
 
 async function handleRequest(request) {
   return new Response(js, {
